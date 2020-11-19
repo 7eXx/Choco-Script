43016780246f1a3ce66a7c35c86ae147404a75b3
Mark <marco.tex@live.com>
2020-11-19 09:17:07 +0100

    Added script with list apps to install.
    Added script list apps and script to install using chocolatey

---
+++ AppInstaller.ps1
@@ -0,0 +1,12 @@
+
+# list of applications to install
+$chocolateyAppList = "googlechrome, firefox, opera, 
+                        7zip, adobereader, libreoffice-fresh, thunderbird, keepass,
+                        vlc, spotify, k-litecodecpackfull, freecommander-xe.install,
+                        vnc-viewer, teamviewer,
+                        imgburn, paint.net, gimp, sharex, google-backup-and-sync,
+                        openjdk, python, nodejs, powershell-core,
+                        winscp, putty, git, eclipse-java-oxygen, vscode,
+                        notepadplusplus.install, sublimetext3"
+
+Invoke-Expression ".\InstallApps.ps1 ""$chocolateyAppList"""
---
+++ InstallApps.ps1
@@ -0,0 +1,25 @@
+Param(  
+    [string]$chocolateyAppList
+)
+
+if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false -or [string]::IsNullOrWhiteSpace($dismAppList) -eq $false)
+{
+    try{
+        choco config get cacheLocation
+    }catch{
+        Write-Output "Chocolatey not detected, trying to install now"
+        iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    }
+}
+
+if ([string]::IsNullOrWhiteSpace($chocolateyAppList) -eq $false){   
+    Write-Host "Chocolatey Apps Specified"  
+    
+    $appsToInstall = $chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+
+    foreach ($app in $appsToInstall)
+    {
+        Write-Host "Installing $app"
+        & choco upgrade $app /y | Write-Output
+    }
+}
