9de128dac22b5c231dd8bf9027c7508be1ae6e9d
Mark <marco.tex@live.com>
2020-11-19 09:24:47 +0100

    Updated list application
    Added visual code insiders to app list

--- AppInstaller.ps1
+++ AppInstaller.ps1
@@ -6,7 +6,7 @@ $chocolateyAppList = "googlechrome, firefox, opera,
                         vnc-viewer, teamviewer,
                         imgburn, paint.net, gimp, sharex, google-backup-and-sync,
                         openjdk, python, nodejs, powershell-core,
-                        winscp, putty, git, eclipse-java-oxygen, vscode,
+                        winscp, putty, git, eclipse-java-oxygen, vscode, vscode-insiders
                         notepadplusplus.install, sublimetext3"
 
 Invoke-Expression ".\InstallApps.ps1 ""$chocolateyAppList"""
