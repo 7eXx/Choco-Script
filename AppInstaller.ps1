b83ebd5b5e770e33980e19253ee6e389bce013d5
Marco.Tessari <marco.tessari@frigoveneta.it>
2021-01-22 13:00:07 +0100

    formatted install list.

--- AppInstaller.ps1
+++ AppInstaller.ps1
@@ -1,13 +1,28 @@
 
 # list of applications to install
-$chocolateyAppList = "googlechrome, firefox, opera, 
-                        7zip, adobereader, libreoffice-fresh, thunderbird, keepass,
-                        vlc, spotify, k-litecodecpackfull, freecommander-xe.install,
-                        vnc-viewer, teamviewer,
-                        imgburn, paint.net, gimp, sharex, google-backup-and-sync,
-                        openjdk, python, nodejs, powershell-core,
-                        winscp, putty, git, eclipse-java-oxygen, vscode, vscode-insiders,
-                        notepadplusplus.install, sublimetext3,
-                        hackfont, cascadiacode, firacode"
+$chocolateyAppList = "";
 
-Invoke-Expression ".\InstallApps.ps1 ""$chocolateyAppList"""
+# web browser
+$chocolateyAppList += "googlechrome, firefox, opera, ";
+
+# utility office
+$chocolateyAppList += "7zip, adobereader, libreoffice-fresh, thunderbird, keepass, ";
+
+# multimedia programs
+$chocolateyAppList += "vlc, spotify, k-litecodecpackfull, imgburn, paint.net, gimp, sharex, google-backup-and-sync, ";
+
+# other utilitis 
+$chocolateyAppList += "freecommander-xe.install, vnc-viewer, teamviewer, winscp, putty, ";
+                        
+# dev tools and platform
+$chocolateyAppList += "openjdk, python, nodejs, git, powershell-core, ";
+
+# ide and editor
+$chocolateyAppList += "eclipse-java-oxygen, vscode, vscode-insiders, notepadplusplus.install, sublimetext3, ";
+                        
+# fonts
+$chocolateyAppList += "hackfont, cascadiacode, firacode";
+                        
+Write-Host $chocolateyAppList;
+
+Invoke-Expression ".\InstallApps.ps1 ""$chocolateyAppList""";
