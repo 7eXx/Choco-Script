740d7aed445037ccc18ae80200c4a62ffca6bbc3
Mark <marco.tex@live.com>
2021-03-24 15:58:34 +0100

    Update AppInstaller.ps1
    adjusted some default apps

--- AppInstaller.ps1
+++ AppInstaller.ps1
@@ -12,7 +12,7 @@ $chocolateyAppList += "7zip, adobereader, libreoffice-fresh, thunderbird, keepas
 $chocolateyAppList += "vlc, spotify, k-litecodecpackfull, imgburn, paint.net, gimp, sharex, google-backup-and-sync, ";
 
 # other utilitis 
-$chocolateyAppList += "freecommander-xe.install, vnc-viewer, teamviewer, winscp, putty, ";
+$chocolateyAppList += "rufus, etcher, vnc-viewer, teamviewer, winscp, putty, ";
                         
 # dev tools and platform
 $chocolateyAppList += "microsoft-windows-terminal, powershell-core, openjdk, python, nodejs, git, tortoisegit, ";
